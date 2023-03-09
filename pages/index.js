c87812ce656f8e153e19ecdf7de2bc77a862e21f
Zelphire <vivekprajeev@gmail.com>
2023-03-09 11:55:25 +0530

    added background

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import InputElement from "@components/InputElement";
+import styles from "../components/Header.module.css";
 import SelectDropDown from "@components/SelectDropDown";
 
 const PLATFORM_OPTIONS = [
@@ -10,16 +11,34 @@ const PLATFORM_OPTIONS = [
   { id: "video", value: "Youtube", label: "Youtube" },
   { id: "tiktok", value: "tiktok", label: "TikTok" },
 ];
+const SUGGESTION_OPTIONS = [
+  { id: 1, value: 5, label: "5" },
+  { id: 2, value: 10, label: "10" },
+  { id: 3, value: 15, label: "15" },
+  { id: 4, value: 20, label: "20" },
+  { id: 5, value: 25, label: "25" },
+  { id: 6, value: 50, label: "50" },
+];
 export default function Home() {
   return (
     <div className="container">
+      <img
+        src="/Chibibanner.png"
+        alt="Chibi Banner"
+        className={styles.bannerImage}
+      />
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Steam title generator (Beta)!" />
+      <main className={styles.main}>
+        <Header title="Steam title generator (Beta)!" />{" "}
+        <SelectDropDown
+          label="Platform"
+          id="platform"
+          options={PLATFORM_OPTIONS}
+        />
         <InputElement
           label="Game/category"
           type="text"
@@ -34,9 +53,9 @@ export default function Home() {
           placeholder="Eg : chaotic stream with friends, sus"
         />
         <SelectDropDown
-          label="Platform"
-          id="platform"
-          options={PLATFORM_OPTIONS}
+          label="Number of Suggestions"
+          id="suggestions"
+          options={SUGGESTION_OPTIONS}
         />
       </main>
       <Footer />
